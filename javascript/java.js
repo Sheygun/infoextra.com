7a95ada6ea6366a85783fb50be71b802189bd036
Olabayo Segun <hollasheg@gmail.com>
2018-08-31 15:40:09 +0100

    i make the web responsive to mobile, tablet, and any screen size

--- javascript/java.js
+++ javascript/java.js
@@ -1,14 +1,18 @@
 
 function post_comment(){
-	let inputName1 = document.getElementById('name').value;
-	let userName1 = document.getElementById('userName1');
-	let inputPost1 = document.getElementById('textarea').value;
-	let userPost1 = document.getElementById('userPost1');
+// 	let inputName1 = document.getElementById('name').value;
+// 	let userName1 = document.getElementById('userName1');
+// 	let inputPost1 = document.getElementById('textarea').value;
+// 	let userPost1 = document.getElementById('userPost1');
 
-	userName1.innerHTML = inputName1;
-	userPost1.innerHTML = inputPost1;
-	let d = new Date();
-	document.getElementById('date').innerHTML = d.ge();
-	let clear1 = document.getElementById('name').value = '';
-	let clear2 = document.getElementById('textarea').value = '';
+// 	userName1.innerHTML = inputName1;
+// 	userPost1.innerHTML = inputPost1;
+// 	let d = new Date();
+// 	document.getElementById('date').innerHTML = d.ge();
+// 	let clear1 = document.getElementById('name').value = '';
+// 	let clear2 = document.getElementById('textarea').value = '';
+	}
+let loginRegister = document.getElementById('login_register');
+		loginRegister.onclick = function(){
+		window.open('login_registration/loginForm.html');
 }
